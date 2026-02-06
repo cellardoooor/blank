36f3c6b94c9fa7c3e909339c730a667b661a3fbe
CellarDoooor <root@HOME-PC.localdomain>
2026-02-06 08:19:47 +0300

    test lb

--- terraform/network/subnet.tf
+++ terraform/network/subnet.tf
@@ -6,12 +6,24 @@ resource "yandex_vpc_subnet" "public" {
   v4_cidr_blocks = [var.public_subnet_cidr]
 }
 
-# Application subnet for Instance Group
+# Route table for NAT Gateway (used by app subnet)
+resource "yandex_vpc_route_table" "nat" {
+  name       = "${var.vpc_name}-nat-route"
+  network_id = yandex_vpc_network.main.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat.id
+  }
+}
+
+# Application subnet for Instance Group (with NAT for internet access)
 resource "yandex_vpc_subnet" "app" {
   name           = var.app_subnet_name
   zone           = var.zone
   network_id     = yandex_vpc_network.main.id
   v4_cidr_blocks = [var.app_subnet_cidr]
+  route_table_id = yandex_vpc_route_table.nat.id
 }
 
 # Database subnet for Managed PostgreSQL
--- terraform/network/vpc.tf
+++ terraform/network/vpc.tf
@@ -1,3 +1,9 @@
 resource "yandex_vpc_network" "main" {
   name = var.vpc_name
 }
+
+# NAT Gateway for app subnet internet access
+resource "yandex_vpc_gateway" "nat" {
+  name = "${var.vpc_name}-nat-gateway"
+  shared_egress_gateway {}
+}
