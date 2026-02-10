8a59064549f84f1acc36852620124c22a5d0020f
CellarDoooor <root@HOME-PC.localdomain>
2026-02-10 23:55:48 +0300

    split envs, move to 1 vm

--- terraform/envs/min/main.tf
+++ terraform/envs/min/main.tf
@@ -1,9 +1,21 @@
-data "yandex_vpc_network" "main" {
-  name = "${var.environment}-messenger-vpc"
-}
+# Network module - creates VPC and subnet
+module "network" {
+  source = "../../network"
 
-data "yandex_vpc_subnet" "app" {
-  name = "${var.environment}-messenger-app"
+  vpc_name = "${var.environment}-messenger-vpc"
+  vpc_cidr = "10.0.0.0/16"
+  zone     = var.zone
+
+  public_subnet_name = "${var.environment}-messenger-public"
+  public_subnet_cidr = "10.0.1.0/24"
+  app_subnet_name    = "${var.environment}-messenger-app"
+  app_subnet_cidr    = "10.0.2.0/24"
+  db_subnet_name     = "${var.environment}-messenger-db"
+  db_subnet_cidr     = "10.0.3.0/24"
+
+  alb_sg_name = "${var.environment}-messenger-alb-sg"
+  app_sg_name = "${var.environment}-messenger-app-sg"
+  db_sg_name  = "${var.environment}-messenger-db-sg"
 }
 
 data "yandex_compute_image" "ubuntu" {
@@ -12,7 +24,7 @@ data "yandex_compute_image" "ubuntu" {
 
 resource "yandex_vpc_security_group" "min_vm" {
   name        = "${var.environment}-min-vm-sg"
-  network_id  = data.yandex_vpc_network.main.id
+  network_id  = module.network.vpc_id
   description = "Security group for min VM"
 
   ingress {
@@ -108,7 +120,7 @@ resource "yandex_compute_instance" "min_vm" {
   }
 
   network_interface {
-    subnet_id          = data.yandex_vpc_subnet.app.id
+    subnet_id          = module.network.app_subnet_id
     security_group_ids = [yandex_vpc_security_group.min_vm.id]
     nat                = true
     nat_ip_address     = yandex_vpc_address.static_ip.external_ipv4_address[0].address
